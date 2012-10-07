e74fc8f3fb8f3c7864430c235b52852067415185
Ryan Stecker <voidedweasel@gmail.com>
2012-10-07 18:03:05 -0500

    Initial commit.

---
+++ extension.cpp
@@ -0,0 +1,181 @@
+
+
+#include "extension.h"
+
+#include "sourcehook.h"
+#include "detours.h"
+
+#include "vphysics_interface.h"
+#include "cbase.h"
+#include "ihandleentity.h"
+
+#include "tier1/strtools.h"
+
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+
+
+CollisionHook g_CollisionHook;
+SMEXT_LINK( &g_CollisionHook );
+
+
+SH_DECL_HOOK0( IPhysics, CreateEnvironment, SH_NOATTRIB, 0 , IPhysicsEnvironment * );
+SH_DECL_HOOK1_void( IPhysicsEnvironment, SetCollisionSolver, SH_NOATTRIB, 0, IPhysicsCollisionSolver * );
+SH_DECL_HOOK4( IPhysicsCollisionSolver, ShouldCollide, SH_NOATTRIB, 0, int, IPhysicsObject *, IPhysicsObject *, void *, void * );
+
+
+IGameConfig *g_pGameConf = NULL;
+CDetour *g_pFilterDetour = NULL;
+
+IPhysics *g_pPhysics = NULL;
+
+IForward *g_pCollisionFwd = NULL;
+IForward *g_pPassFwd = NULL;
+
+
+DETOUR_DECL_STATIC2( PassServerEntityFilterFunc, bool, const IHandleEntity *, pTouch, const IHandleEntity *, pPass )
+{
+	if ( g_pPassFwd->GetFunctionCount() == 0 )
+		return DETOUR_STATIC_CALL( PassServerEntityFilterFunc )( pTouch, pPass );
+
+	if ( pTouch == pPass )
+		return DETOUR_STATIC_CALL( PassServerEntityFilterFunc )( pTouch, pPass ); // self checks aren't interesting
+
+	CBaseEntity *pEnt1 = const_cast<CBaseEntity *>( UTIL_EntityFromEntityHandle( pTouch ) );
+	CBaseEntity *pEnt2 = const_cast<CBaseEntity *>( UTIL_EntityFromEntityHandle( pPass ) );
+
+	if ( !pEnt1 || !pEnt2 )
+		return DETOUR_STATIC_CALL( PassServerEntityFilterFunc )( pTouch, pPass ); // we need both entities
+
+	cell_t ent1 = gamehelpers->EntityToBCompatRef( pEnt1 );
+	cell_t ent2 = gamehelpers->EntityToBCompatRef( pEnt2 );
+
+	cell_t result = 0;
+	g_pPassFwd->PushCell( ent1 );
+	g_pPassFwd->PushCell( ent2 );
+	g_pPassFwd->PushCellByRef( &result );
+
+	cell_t retValue = 0;
+	g_pPassFwd->Execute( &retValue );
+
+	if ( retValue > Pl_Continue )
+	{
+		// plugin wants to change the result
+		return result == 1;
+	}
+
+	return DETOUR_STATIC_CALL( PassServerEntityFilterFunc )( pTouch, pPass );
+}
+
+
+bool CollisionHook::SDK_OnLoad( char *error, size_t maxlength, bool late )
+{
+	sharesys->RegisterLibrary( myself, "collisionhook" );
+
+	char szConfError[ 256 ] = "";
+	if ( !gameconfs->LoadGameConfigFile( "collisionhook", &g_pGameConf, szConfError, sizeof( szConfError ) ) )
+	{
+		V_snprintf( error, maxlength, "Could not read collisionhook gamedata: %s", szConfError );
+		return false;
+	}
+
+	CDetourManager::Init( g_pSM->GetScriptingEngine(), g_pGameConf );
+
+	g_pFilterDetour = DETOUR_CREATE_STATIC( PassServerEntityFilterFunc, "PassServerEntityFilter" );
+	if ( !g_pFilterDetour )
+	{
+		V_snprintf( error, maxlength, "Unable to hook PassServerEntityFilter!" );
+		return false;
+	}
+
+	g_pFilterDetour->EnableDetour();
+
+	g_pCollisionFwd = forwards->CreateForward( "CH_ShouldCollide", ET_Hook, 3, NULL, Param_Cell, Param_Cell, Param_CellByRef );
+	g_pPassFwd = forwards->CreateForward( "CH_PassFilter", ET_Hook, 3, NULL, Param_Cell, Param_Cell, Param_CellByRef );
+
+	return true;
+}
+
+void CollisionHook::SDK_OnUnload()
+{
+	forwards->ReleaseForward( g_pCollisionFwd );
+	forwards->ReleaseForward( g_pPassFwd );
+
+	gameconfs->CloseGameConfigFile( g_pGameConf );
+
+	if ( g_pFilterDetour )
+	{
+		g_pFilterDetour->Destroy();
+		g_pFilterDetour = NULL;
+	}
+}
+
+bool CollisionHook::SDK_OnMetamodLoad( ISmmAPI *ismm, char *error, size_t maxlen, bool late )
+{
+	GET_V_IFACE_CURRENT( GetPhysicsFactory, g_pPhysics, IPhysics, VPHYSICS_INTERFACE_VERSION );
+
+	SH_ADD_HOOK( IPhysics, CreateEnvironment, g_pPhysics, SH_MEMBER( this, &CollisionHook::CreateEnvironment ), false );
+
+	return true;
+}
+
+bool CollisionHook::SDK_OnMetamodUnload(char *error, size_t maxlength)
+{
+	SH_REMOVE_HOOK( IPhysics, CreateEnvironment, g_pPhysics, SH_MEMBER( this, &CollisionHook::CreateEnvironment ), false );
+
+	g_pPhysics = NULL;
+
+	return true;
+}
+
+
+IPhysicsEnvironment *CollisionHook::CreateEnvironment()
+{
+	IPhysicsEnvironment *pEnvironment = SH_CALL( g_pPhysics, &IPhysics::CreateEnvironment )();
+	SH_ADD_HOOK( IPhysicsEnvironment, SetCollisionSolver, pEnvironment, SH_MEMBER( this, &CollisionHook::SetCollisionSolver ), false );
+
+	RETURN_META_VALUE( MRES_SUPERCEDE, pEnvironment );
+}
+
+void CollisionHook::SetCollisionSolver( IPhysicsCollisionSolver *pSolver )
+{
+	SH_ADD_HOOK( IPhysicsCollisionSolver, ShouldCollide, pSolver, SH_MEMBER( this, &CollisionHook::VPhysics_ShouldCollide ), false );
+
+	RETURN_META( MRES_IGNORED );
+}
+
+int CollisionHook::VPhysics_ShouldCollide( IPhysicsObject *pObj1, IPhysicsObject *pObj2, void *pGameData1, void *pGameData2 )
+{
+	if ( g_pCollisionFwd->GetFunctionCount() == 0 )
+		RETURN_META_VALUE( MRES_IGNORED, 1 ); // no plugins are interested, let the game decide
+
+	if ( pObj1 == pObj2 )
+		RETURN_META_VALUE( MRES_IGNORED, 1 ); // self collisions aren't interesting
+
+	CBaseEntity *pEnt1 = reinterpret_cast<CBaseEntity *>( pGameData1 );
+	CBaseEntity *pEnt2 = reinterpret_cast<CBaseEntity *>( pGameData2 );
+
+	if ( !pEnt1 || !pEnt2 )
+		RETURN_META_VALUE( MRES_IGNORED, 1 ); // we need two entities
+
+	cell_t ent1 = gamehelpers->EntityToBCompatRef( pEnt1 );
+	cell_t ent2 = gamehelpers->EntityToBCompatRef( pEnt2 );
+
+	cell_t result = 0;
+	g_pCollisionFwd->PushCell( ent1 );
+	g_pCollisionFwd->PushCell( ent2 );
+	g_pCollisionFwd->PushCellByRef( &result );
+
+	cell_t retValue = 0;
+	g_pCollisionFwd->Execute( &retValue );
+
+	if ( retValue > Pl_Continue )
+	{
+		// plugin wants to change the result
+		RETURN_META_VALUE( MRES_SUPERCEDE, result == 1 );
+	}
+
+	RETURN_META_VALUE( MRES_IGNORED, 0 );
+}
---
+++ extension.h
@@ -0,0 +1,103 @@
+
+#ifndef _INCLUDE_COLLISIONHOOK_EXTENSION_H_
+#define _INCLUDE_COLLISIONHOOK_EXTENSION_H_
+
+
+#include "smsdk_ext.h"
+
+
+class IPhysicsEnvironment;
+class IPhysicsCollisionSolver;
+class IPhysicsObject;
+
+
+class CollisionHook :
+	public SDKExtension
+{
+
+public:
+	/**
+	 * @brief This is called after the initial loading sequence has been processed.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlength	Size of error message buffer.
+	 * @param late		Whether or not the module was loaded after map load.
+	 * @return			True to succeed loading, false to fail.
+	 */
+	virtual bool SDK_OnLoad( char *error, size_t maxlength, bool late );
+	
+	/**
+	 * @brief This is called right before the extension is unloaded.
+	 */
+	virtual void SDK_OnUnload();
+
+	/**
+	 * @brief This is called once all known extensions have been loaded.
+	 * Note: It is is a good idea to add natives here, if any are provided.
+	 */
+	//virtual void SDK_OnAllLoaded();
+
+	/**
+	 * @brief Called when the pause state is changed.
+	 */
+	//virtual void SDK_OnPauseChange(bool paused);
+
+	/**
+	 * @brief this is called when Core wants to know if your extension is working.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlength	Size of error message buffer.
+	 * @return			True if working, false otherwise.
+	 */
+	//virtual bool QueryRunning(char *error, size_t maxlength);
+public:
+#if defined SMEXT_CONF_METAMOD
+	/**
+	 * @brief Called when Metamod is attached, before the extension version is called.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlength		Maximum size of error buffer.
+	 * @param late			Whether or not Metamod considers this a late load.
+	 * @return				True to succeed, false to fail.
+	 */
+	virtual bool SDK_OnMetamodLoad( ISmmAPI *ismm, char *error, size_t maxlength, bool late );
+
+	/**
+	 * @brief Called when Metamod is detaching, after the extension version is called.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlength		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	virtual bool SDK_OnMetamodUnload(char *error, size_t maxlength);
+
+	/**
+	 * @brief Called when Metamod's pause state is changing.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param paused		Pause state being set.
+	 * @param error			Error buffer.
+	 * @param maxlength		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlength);
+#endif
+
+public: // hooks
+	IPhysicsEnvironment *CreateEnvironment();
+	void SetCollisionSolver( IPhysicsCollisionSolver *pSolver );
+	int VPhysics_ShouldCollide( IPhysicsObject *pObj1, IPhysicsObject *pObj2, void *pGameData1, void *pGameData2 );
+
+};
+
+inline const CBaseEntity *UTIL_EntityFromEntityHandle( const IHandleEntity *pConstHandleEntity )
+{
+	IHandleEntity *pHandleEntity = const_cast<IHandleEntity *>( pConstHandleEntity );
+	IServerUnknown *pUnk = static_cast<IServerUnknown *>( pHandleEntity );
+
+	return pUnk->GetBaseEntity();
+}
+
+
+#endif // _INCLUDE_COLLISIONHOOK_EXTENSION_H_
